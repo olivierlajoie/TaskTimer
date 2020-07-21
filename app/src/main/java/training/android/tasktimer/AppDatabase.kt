87f6e9b3c8c7759953576f1f7abee53712f38605
Olivier Lajoie <olivier.lajoie@dgaginno.com>
2020-07-21 15:44:41 -0400

    Content Provider & URI

--- app/src/main/java/training/android/tasktimer/AppDatabase.kt
+++ app/src/main/java/training/android/tasktimer/AppDatabase.kt
@@ -9,29 +9,34 @@ private const val TAG = "AppDatabase"
 private const val DATABASE_NAME = "TaskTimer.db"
 private const val DATABASE_VERSION = 3
 
-internal class AppDatabase constructor(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+internal class AppDatabase private constructor(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
 
     init { Log.d(TAG, "AppDatabase: init") }
 
     override fun onCreate(db: SQLiteDatabase?) {
         //CREATE TABLE Tasks (_id INTEGER PRIMARY KEY NOT NULL, name TEXT NOT NULL, desc TEXT, sortOrder INTEGER);
-        Log.d(TAG, "OnCreate: starts")
+        Log.d(TAG, "onCreate: starts")
 
         val sSQL = """CREATE TABLE ${TasksContract.TABLE_NAME} (
             |${TasksContract.Columms.ID} INTEGER PRIMARY KEY NOT NULL,
             |${TasksContract.Columms.TASK_NAME} TEXT NOT NULL,
             |${TasksContract.Columms.TASK_DESC} TEXT,
-            |${TasksContract.Columms.TASK_ORDER} INTEGER,
+            |${TasksContract.Columms.TASK_ORDER} INTEGER
             |)""".trimMargin()
 
         Log.d(TAG, sSQL)
 
         db?.execSQL(sSQL)
-
-        Log.d(TAG,"WTF")
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        TODO("Not yet implemented")
+        Log.d(TAG, "onUpgrade: starts")
+
+        when(oldVersion) {
+            1 -> {/*upgrade logic*/}
+            else -> throw IllegalStateException("onUpgrade() with unknown newVersion $newVersion")
+        }
     }
+
+    companion object : SingletonHolder<AppDatabase, Context>(::AppDatabase)
 }
---
+++ app/src/main/java/training/android/tasktimer/AppProvider.kt
@@ -0,0 +1,63 @@
+package training.android.tasktimer
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.content.UriMatcher
+import android.database.Cursor
+import android.net.Uri
+import android.util.Log
+
+private const val TAG = "AppProvier"
+private val CONTENT_AUTHORITY = "training.android.tasktimer.provider"
+
+private const val TASKS = 100
+private const val TASKS_ID = 101
+
+private const val TIMINGS = 200
+private const val TIMINGS_ID = 201
+
+private const val TASK_DURATIONS = 400
+private const val TASK_DURATIONS_ID = 401
+
+val CONTENT_AUTHORITY_URI : Uri = Uri.parse("content://$CONTENT_AUTHORITY")
+
+class AppProvider : ContentProvider() {
+
+    private val uriMatcher by lazy { buildUriMatcher() }
+
+    private fun buildUriMatcher() : UriMatcher {
+        Log.d(TAG, "buildUriMatcher starts")
+        val matcher = UriMatcher(UriMatcher.NO_MATCH)
+        return matcher
+    }
+
+    override fun insert(p0: Uri, p1: ContentValues?): Uri? {
+        TODO("Not yet implemented")
+    }
+
+    override fun query(
+        p0: Uri,
+        p1: Array<out String>?,
+        p2: String?,
+        p3: Array<out String>?,
+        p4: String?
+    ): Cursor? {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreate(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(p0: Uri, p1: String?, p2: Array<out String>?): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getType(p0: Uri): String? {
+        TODO("Not yet implemented")
+    }
+}
